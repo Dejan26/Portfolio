3c1ab98bd1a14c131e18a0cd63fd8eb9ae5965b5
Dejan <dejanmirkovic01@gmail.com>
2025-08-10 00:26:15 +0200

    Initial commit

---
+++ app/layout.js
@@ -0,0 +1,23 @@
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"],weight:["400", "500", "600", "700"],variable:"--font-Outfit"
+});
+
+
+const ovo = Ovo({
+  subsets: ["latin"],weight:["400"],variable:"--font-Ovo"
+});
+
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="scroll-smooth dark">
+      <body className={`${outfit.variable} ${ovo.variable} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}>
+        {children}
+      </body>
+    </html>
+  );
+}
