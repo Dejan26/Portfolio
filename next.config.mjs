e45f576164bcacd24b74fe82c60db206b77c0a73
Dejan <dejanmirkovic01@gmail.com>
2025-08-11 10:33:13 +0200

    Promena za export za static sajt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = { output: 'standalone',};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'standalone',};
+const nextConfig = {
+  output: 'export', // umesto 'standalone'
+};
 
 export default nextConfig;
